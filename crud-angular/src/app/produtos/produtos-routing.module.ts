115fdd8ab676928c1915dbcc47f3bafb11558ffc
caiquecampanaro <caiquecampanaro@gmail.com>
2024-11-16 19:36:34 -0300

    customizacao de cores do sistema

--- crud-angular/src/app/produtos/produtos-routing.module.ts
+++ crud-angular/src/app/produtos/produtos-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProdutosComponent } from './produtos/produtos.component';
+
+const routes: Routes = [
+  { path: '', component: ProdutosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
